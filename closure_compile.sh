28345fa1c13d56fcea86eb08b8edd252ceac82ef
acerix <dylan@psilly.com>
2013-03-24 22:21:30 -0400

    rebuild

--- closure_compile.sh
+++ closure_compile.sh
@@ -1,7 +1,11 @@
 closure --js \
 src/integer.js \
+src/biginteger.js \
+src/bigint.js \
 src/rat.js \
+src/bigrat.js \
 src/rational.js \
+src/physics.js \
 src/deprecated.js \
 > dist/rational.js
 
